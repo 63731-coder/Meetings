0a86cc83bb0c4a11e712fb3e75f5d7927f045ac6
G63737 <63737@etu.he2b.be>
2025-12-07 16:58:11 +0100

    correction pom.xml & end package meeting

--- rencontres/rencontres/src/main/java/be/esi/rencontres/meeting/model/neo4j/MeetingRelationship.java
+++ rencontres/rencontres/src/main/java/be/esi/rencontres/meeting/model/neo4j/MeetingRelationship.java
@@ -1,11 +1,36 @@
 package be.esi.rencontres.meeting.model.neo4j;
 
+import be.esi.rencontres.user.model.neo4j.UserNode;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.RelationshipProperties;
+import org.springframework.data.neo4j.core.schema.TargetNode;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@RelationshipProperties
 public class MeetingRelationship {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private LocalDateTime meetingDate;
+
+    private String location;
+
+    @TargetNode
+    private UserNode targetUser;
+
+    public MeetingRelationship(LocalDateTime meetingDate, String location, UserNode targetUser) {
+        this.meetingDate = meetingDate;
+        this.location = location;
+        this.targetUser = targetUser;
+    }
 }
